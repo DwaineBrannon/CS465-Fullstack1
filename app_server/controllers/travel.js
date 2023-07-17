5e0856eb6405a9cf2a5f6809fc207ceac5db3ef6
Dwaine Brannon <dwainebrannon@gmail.com>
2023-07-17 19:07:26 -0400

    Convert Travel template from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,7 +1,13 @@
-//To get /travel in the url
+const fs = require('fs');
+
+// Take JSON file, read as string, parse and convert to JSON
+// Not best practice! Best practice would be to read on startup and cache files
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+// To get /travel in the url
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title: pageTitle});
+    res.render('travel', {title: pageTitle, trips});
 };
 
 module.exports = {
