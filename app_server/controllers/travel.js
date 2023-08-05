242b0712ca597e3da655da1274b3353d23b67936
Dwaine Brannon <dwainebrannon@gmail.com>
2023-08-05 16:27:26 -0400

    renderTravelList added

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -3,6 +3,30 @@ const apiOptions = {
     server: 'https://localhost:3000'
 }
 
+/* render travel list view */
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - Travel';
+
+    // if the result was not an array, make it an array and report error
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    }
+    else {
+        if (!responseBody.length) {
+            message = 'No trips exist in database';
+        }
+    }
+
+    // render the page
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
+};
+
 // To get /travel in the url
 const travelList = (req, res) => {
     const path = '/api/trips';
