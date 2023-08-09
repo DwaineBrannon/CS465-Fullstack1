70e5ecf46ff40e1cc2fa424860530a0c2805e816
Dwaine Brannon <dwainebrannon@gmail.com>
2023-08-09 19:25:34 -0400

    Added SPA in Angular

--- app.js
+++ app.js
@@ -29,6 +29,14 @@ app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
 app.use('/api', apiRouter);
 
+// allow CORS
+app.use('api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requesteed-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
